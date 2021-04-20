27e86955cb1069046429a5c0d13e19daf6d7a956
Donald Mahoro ntwari <dmahoro-@c1r8p1.42nice.fr>
2021-04-20 23:28:17 +0200

    corrected ft_itoa_base, removed set but not used

--- ft_itoa_base.c
+++ ft_itoa_base.c
@@ -6,7 +6,7 @@
 /*   By: dmahoro- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/02 04:17:06 by dmahoro-          #+#    #+#             */
-/*   Updated: 2021/04/02 13:57:59 by dmahoro-         ###   ########.fr       */
+/*   Updated: 2021/04/20 23:25:53 by dmahoro-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,14 +34,12 @@ static char	*ft_fillstr(unsigned int numb, size_t i, size_t neg, int base)
 
 char	*ft_itoa_base(int n, int base)
 {
-	char			*b;
 	unsigned int	numb;
 	unsigned int	buff;
 	size_t			i;
 
 	if (base < 2 || base > 16)
 		return (NULL);
-	b = "0123456789ABCDEF";
 	if (n < 0)
 		numb = -n;
 	else
