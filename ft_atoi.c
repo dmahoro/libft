bf51e7ece4a40acba8864b81f26615323cb3ed60
Donald Mahoro ntwari <dmahoro-@c1r8p3.42nice.fr>
2021-05-25 12:32:13 +0200

    still norminette fail

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,18 +6,18 @@
 /*   By: dmahoro- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 21:56:06 by dmahoro-          #+#    #+#             */
-/*   Updated: 2021/05/25 09:59:47 by dmahoro-         ###   ########.fr       */
+/*   Updated: 2021/05/25 12:13:17 by dmahoro-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 //# define	FT_ULONG_MAX	((unsigned long)(~0L))
-# define	FT_ULONG_MAX	((unsigned long)~0L)
+#define FT_ULONG_MAX ((unsigned long)~0L)
 //# define	FT_LONG_MAX		((long)(FT_ULONG_MAX >> 1))
-# define	FT_LONG_MAX		((long)(((unsigned long)~0L) >> 1))
+#define FT_LONG_MAX ((long)(((unsigned long)~0L) >> 1))
 //# define	FT_LONG_MIN		((long)(~FT_LONG_MAX))
-# define	FT_LONG_MIN		((long)~(((unsigned long)~0L) >> 1))
+#define FT_LONG_MIN ((long)~(((unsigned long)~0L) >> 1))
 
 static long	handle_limit(long res, int overflow, int sign)
 {
